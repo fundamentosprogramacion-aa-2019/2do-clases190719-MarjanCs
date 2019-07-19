cfcb7b2de2a67b663517a6a0856000581e36d84b
Marjan Celi <marjanazliga@gmail.com>
2019-07-19 10:30:40 -0500

    Manejo de Constructores con Arreglos

---
+++ ManejoConstructores/src/manejoconstructoresdos/Equipo.java
@@ -0,0 +1,37 @@
+
+package manejoconstructoresdos;
+
+
+public class Equipo {
+    private String nombre;
+    private Ciudad ciudad;
+    
+    public Equipo(String n, Ciudad c){
+        establecerNombre(n.toUpperCase());
+        establecerCiudad(c);
+        
+    }
+    public void establecerNombre(String v){
+        nombre = v;
+    }
+    
+    public void establecerCiudad(Ciudad c){
+        ciudad = c;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public Ciudad obtenerCiudad(){
+        return ciudad;
+    }
+    
+    public String toString(){
+        String cadena = String.format("Equipo:\n\tNombre:%s\n\tCiudad:%s\n", 
+                obtenerNombre(), obtenerCiudad().obtenerNombre());
+        
+        return cadena;
+    }
+   
+}
